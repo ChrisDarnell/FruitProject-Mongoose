fb4fd51144845a50b30fac60cd6c22270355b900
Chris <chrisdarnell@gmail.com>
2021-06-09 21:28:02 -0500

    Mongoose Init Complete

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
 //jshint esversion:6
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'fruitsDB';
-
-// Create a new MongoClient
-const client = new MongoClient(url, {useUnifiedTopology: true});
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-
-    findDocuments(db, function() {
-      client.close();
-    });
-  });
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useUnifiedTopology: true, useNewUrlParser: true });
 
 // New Collection - Fruits
 
